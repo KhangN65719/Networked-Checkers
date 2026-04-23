915d39639cc9c5780bbf197f1aadc89fdb71a7ee
Khang Nguyen <Winnguyen0919@gmail.com>
2026-04-23 11:58:49 -0500

    Server side logic for piece movement and forfeits

--- HW5Server/src/main/java/Message.java
+++ HW5Server/src/main/java/Message.java
@@ -15,82 +15,141 @@ public class Message implements Serializable {
     public static final int CHAT_MESSAGE = 9;
     public static final int GROUP_LIST = 10;
     public static final int JOIN_GROUP = 11;
+    public static final int WAITING = 12;
+    public static final int GAME_START = 13;
+    public static final int GAME_OVER = 14;
+    public static final int PLAY_AGAIN = 15;
+    public static final int DISCONNECT_QUEUE = 16;
+    public static final int GAME_MOVE = 17;
+    public static final int FORFEIT = 18;
+    public static final int OPPONENT_FORFEIT = 19;
 
     public int type;
     public String sender;
     public String recipient;
     public String groupName;
     public String content;
+    public int connectedCount;
+    public int fromRow, fromCol, toRow, toCol;
     public ArrayList<String> userList;
     public ArrayList<String> groupList;
 
     public Message() {}
 
     public static Message signIn(String username) {
-        Message msg = new Message();
-        msg.type = SIGN_IN;
-        msg.sender = username;
-        return msg;
+        Message m = new Message();
+        m.type = SIGN_IN;
+        m.sender = username;
+        return m;
     }
     public static Message sendAll(String sender, String content) {
-        Message msg = new Message();
-        msg.type = SEND_ALL;
-        msg.sender = sender;
-        msg.content = content;
-        return msg;
+        Message m = new Message();
+        m.type = SEND_ALL;
+        m.sender = sender;
+        m.content = content;
+        return m;
     }
     public static Message sendPrivate(String sender, String recipient, String content) {
-        Message msg = new Message();
-        msg.type = SEND_PRIVATE;
-        msg.sender = sender;
-        msg.recipient = recipient;
-        msg.content = content;
-        return msg;
+        Message m = new Message();
+        m.type = SEND_PRIVATE;
+        m.sender = sender;
+        m.recipient = recipient;
+        m.content = content;
+        return m;
     }
     public static Message createGroup(String sender, String groupName) {
-        Message msg = new Message();
-        msg.type = CREATE_GROUP;
-        msg.sender = sender;
-        msg.groupName = groupName;
-        return msg;
+        Message m = new Message();
+        m.type = CREATE_GROUP;
+        m.sender = sender;
+        m.groupName = groupName;
+        return m;
     }
     public static Message joinGroup(String sender, String groupName) {
-        Message msg = new Message();
-        msg.type = JOIN_GROUP;
-        msg.sender = sender;
-        msg.groupName = groupName;
-        return msg;
+        Message m = new Message();
+        m.type = JOIN_GROUP;
+        m.sender = sender;
+        m.groupName = groupName;
+        return m;
     }
     public static Message sendGroup(String sender, String groupName, String content) {
-        Message msg = new Message();
-        msg.type = SEND_GROUP;
-        msg.sender = sender;
-        msg.groupName = groupName;
-        msg.content = content;
-        return msg;
+        Message m = new Message();
+        m.type = SEND_GROUP;
+        m.sender = sender;
+        m.groupName = groupName;
+        m.content = content;
+        return m;
+    }
+    public static Message forfeit(String sender) {
+        Message m = new Message();
+        m.type = FORFEIT;
+        m.sender = sender;
+        return m;
+    }
+    public static Message playAgain(String sender) {
+        Message m = new Message();
+        m.type = PLAY_AGAIN;
+        m.sender = sender;
+        return m;
+    }
+    public static Message disconnectQueue(String sender) {
+        Message m = new Message();
+        m.type = DISCONNECT_QUEUE;
+        m.sender = sender;
+        return m;
+    }
+    public static Message gameMove(String sender, int fromRow, int fromCol, int toRow, int toCol) {
+        Message m = new Message();
+        m.type = GAME_MOVE;
+        m.sender = sender;
+        m.fromRow = fromRow;
+        m.fromCol = fromCol;
+        m.toRow = toRow;
+        m.toCol = toCol;
+        return m;
     }
 
     @Override
     public String toString() {
         switch (type) {
-            case SIGN_IN_OK:
+            case SIGN_IN_OK:{
                 return "[Accepted] Welcome, " + sender + "!";
-            case SIGN_IN_FAIL:
+            }
+            case SIGN_IN_FAIL: {
                 return "[Error] Username '" + sender + "' is already taken.";
-            case SEND_ALL:
+            }
+            case SEND_ALL: {
                 return "[All] " + sender + ": " + content;
-            case SEND_PRIVATE:
+            }
+            case SEND_PRIVATE: {
                 return "[DM from " + sender + "]: " + content;
-            case CHAT_MESSAGE:
+            }
+            case CHAT_MESSAGE: {
                 return content;
-            case CREATE_GROUP:
+            }
+            case CREATE_GROUP: {
                 return "[Group '" + groupName + "' created by " + sender + "]";
-            case JOIN_GROUP:
+            }
+            case JOIN_GROUP: {
                 return "[" + sender + " joined group '" + groupName + "']";
-            case SEND_GROUP:
+            }
+            case SEND_GROUP: {
                 return "[Group " + groupName + "] " + sender + ": " + content;
-            default:
+            }
+            case WAITING: {
+                return "[Waiting] " + connectedCount + "/2 connected";
+            }
+            case GAME_START: {
+                return "[Game Start] Opponent: " + recipient;
+            }
+            case GAME_OVER: {
+                return "[Game Over] " + content;
+            }
+            case GAME_MOVE: {
+                return "[Move] " + sender + ": (" + fromRow + "," + fromCol + ") -> (" + toRow + "," + toCol + ")";
+            }
+            default: {
                 return "Message{type=" + type + "}";
+            }
         }
     }
 }
--- HW5Server/src/main/java/Server.java
+++ HW5Server/src/main/java/Server.java
@@ -11,9 +11,12 @@ public class Server {
 	int clientCount = 1;
 	ArrayList<ClientThread> clients = new ArrayList<>();
 	TheServer server;
-	private Consumer<Serializable> callback;
+	private final Consumer<Serializable> callback;
 	Set<String> takenUsernames = new HashSet<>();
 	Map<String, Set<String>> groups = new HashMap<>();
+
+	private final LinkedList<ClientThread> matchQueue = new LinkedList<>();
+	private final Map<String, ClientThread> rematchQueue = new HashMap<>();
 
 	Server(Consumer<Serializable> call) {
 		callback = call;
@@ -21,38 +24,97 @@ public class Server {
 		server.start();
 	}
 
-	private void broadcastUserList() {
+	private synchronized void broadcastUserList() {
 		Message msg = new Message();
 		msg.type = Message.USER_LIST;
 		msg.userList = new ArrayList<>(takenUsernames);
-		for (ClientThread clientThread : clients) clientThread.send(msg);
-	}
-
-	private void broadcastGroupList() {
-		Message msg = new Message();
-		msg.type = Message.GROUP_LIST;
-		msg.groupList = new ArrayList<>(groups.keySet());
-		for (ClientThread clientThread : clients) {
-			clientThread.send(msg);
+		for (ClientThread ct : clients) {
+			ct.send(msg);
 		}
 	}
 
-	private ClientThread findClient(String username) {
-		for (ClientThread clientThread : clients) {
-			if (username.equals(clientThread.username)) return clientThread;
+	private synchronized void broadcastGroupList() {
+		Message msg = new Message();
+		msg.type = Message.GROUP_LIST;
+		msg.groupList = new ArrayList<>(groups.keySet());
+		for (ClientThread ct : clients) {
+			ct.send(msg);
+		}
+	}
+
+	private synchronized ClientThread findClient(String username) {
+		for (ClientThread ct : clients) {
+			if (username.equals(ct.username)) {
+				return ct;
+			}
 		}
 		return null;
 	}
 
+	synchronized void enqueueForMatch(ClientThread ct) {
+		if (matchQueue.contains(ct)) return;
+		matchQueue.add(ct);
+		callback.accept("(Queue) " + ct.username + " is waiting. Queue size: " + matchQueue.size());
+
+		Message waiting = new Message();
+		waiting.type = Message.WAITING;
+		waiting.connectedCount = matchQueue.size();
+		ct.send(waiting);
+
+		if (matchQueue.size() >= 2) {
+			ClientThread p1 = matchQueue.poll();
+			ClientThread p2 = matchQueue.poll();
+			startGame(p1, p2);
+		}
+	}
+
+	private void startGame(ClientThread p1, ClientThread p2) {
+		p1.opponent = p2;
+		p2.opponent = p1;
+		p1.lastOpponentUsername = p2.username;
+		p2.lastOpponentUsername = p1.username;
+		callback.accept("(Match) " + p1.username + " vs " + p2.username);
+
+		Message start1 = new Message();
+		start1.type = Message.GAME_START;
+		start1.sender = p1.username;
+		start1.recipient = p2.username;
+		start1.content = "LIGHT";
+		p1.send(start1);
+
+		Message start2 = new Message();
+		start2.type = Message.GAME_START;
+		start2.sender = p2.username;
+		start2.recipient = p1.username;
+		start2.content = "DARK";
+		p2.send(start2);
+	}
+
+	synchronized void endGame(ClientThread winner, ClientThread loser) {
+		Message win = new Message();
+		win.type = Message.GAME_OVER;
+		win.content = "WIN";
+		winner.send(win);
+
+		Message lose = new Message();
+		lose.type = Message.GAME_OVER;
+		lose.content = "LOSE";
+		loser.send(lose);
+
+		winner.opponent = null;
+		loser.opponent = null;
+		callback.accept("(Game Over) " + winner.username + " beat " + loser.username);
+	}
+
 	public class TheServer extends Thread {
 		public void run() {
-			try (ServerSocket serverSocket = new ServerSocket(5555)) {
+			try (ServerSocket ss = new ServerSocket(5555)) {
 				callback.accept("Server is waiting for clients on port 5555...");
 				while (true) {
-					Socket socket = serverSocket.accept();
-					ClientThread clientThread = new ClientThread(socket, clientCount++);
-					clients.add(clientThread);
-					clientThread.start();
+					Socket socket = ss.accept();
+					ClientThread ct = new ClientThread(socket, clientCount++);
+					clients.add(ct);
+					ct.start();
 				}
 			} catch (Exception e) {
 				callback.accept("Server socket failed: " + e.getMessage());
@@ -64,6 +126,8 @@ public class Server {
 		Socket connection;
 		int id;
 		String username = null;
+		ClientThread opponent = null;
+		String lastOpponentUsername = null;
 		ObjectInputStream inStream;
 		ObjectOutputStream outStream;
 
@@ -74,8 +138,9 @@ public class Server {
 
 		synchronized void send(Message msg) {
 			try {
-				outStream.writeObject(msg); outStream.reset();
-			} catch (Exception e) {}
+				outStream.writeObject(msg);
+				outStream.reset();
+			} catch (Exception ignored) {}
 		}
 
 		public void run() {
@@ -88,8 +153,8 @@ public class Server {
 			}
 			while (true) {
 				try {
-					Message receivedMsg = (Message) inStream.readObject();
-					handleMessage(receivedMsg);
+					Message msg = (Message) inStream.readObject();
+					handleMessage(msg);
 				} catch (Exception e) {
 					handleDisconnect(); break;
 				}
@@ -98,123 +163,204 @@ public class Server {
 
 		private void handleMessage(Message data) {
 			switch (data.type) {
-
 				case Message.SIGN_IN: {
-					String requestedName = data.sender == null ? "" : data.sender.trim();
+					String name = data.sender == null ? "" : data.sender.trim();
 					Message reply = new Message();
-					reply.sender = requestedName;
-					if (requestedName.isEmpty() || takenUsernames.contains(requestedName)) {
+					reply.sender = name;
+					if (name.isEmpty() || takenUsernames.contains(name)) {
 						reply.type = Message.SIGN_IN_FAIL;
 						send(reply);
-						callback.accept("Sign-in REJECTED for '" + requestedName + "'");
+						callback.accept("Sign-in REJECTED for '" + name + "'");
 					} else {
-						takenUsernames.add(requestedName);
-						this.username = requestedName;
+						takenUsernames.add(name);
+						this.username = name;
 						reply.type = Message.SIGN_IN_OK;
 						send(reply);
-						callback.accept("'" + requestedName + "' connected (client #" + id + ")");
+						callback.accept("'" + name + "' connected (client #" + id + ")");
 						broadcastUserList();
 						broadcastGroupList();
 					}
 					break;
 				}
-
+				case Message.WAITING: {
+					enqueueForMatch(this);
+					break;
+				}
+				case Message.DISCONNECT_QUEUE: {
+					synchronized (Server.this) {
+						matchQueue.remove(this);
+					}
+					callback.accept("(Queue) " + username + " left the queue.");
+					break;
+				}
+				case Message.PLAY_AGAIN: {
+					String lastOpp = this.lastOpponentUsername;
+					if (lastOpp == null) {
+						enqueueForMatch(this);
+						break;
+					}
+					ClientThread oppThread = findClient(lastOpp);
+					if (oppThread == null) {
+						Message noOpp = new Message();
+						noOpp.type = Message.GAME_OVER;
+						noOpp.content = "NO_OPPONENT";
+						send(noOpp);
+						callback.accept("(Rematch) " + username + " has no opponent to rematch (disconnected).");
+						break;
+					}
+					synchronized (Server.this) {
+						if (rematchQueue.containsKey(lastOpp) && rematchQueue.get(lastOpp) == oppThread) {
+							rematchQueue.remove(lastOpp);
+							callback.accept("(Rematch) " + username + " and " + lastOpp + " are rematching.");
+							startGame(this, oppThread);
+						}
+						else {
+							rematchQueue.put(username, this);
+							Message waiting = new Message();
+							waiting.type = Message.WAITING;
+							waiting.connectedCount = 1;
+							send(waiting);
+							callback.accept("(Rematch) " + username + " is waiting for " + lastOpp + " to rematch.");
+						}
+					}
+					break;
+				}
+				case Message.GAME_OVER: {
+					synchronized (Server.this) {
+						if (opponent != null) {
+							opponent.opponent = null;
+						}
+						opponent = null;
+					}
+					callback.accept("(Game Over) " + username + " reported game finished.");
+					break;
+				}
+				case Message.FORFEIT: {
+					synchronized (Server.this) {
+						rematchQueue.remove(username);
+						if (lastOpponentUsername != null) {
+							rematchQueue.remove(lastOpponentUsername);
+							ClientThread lastOpp = findClient(lastOpponentUsername);
+							if (lastOpp != null) {
+								Message out = new Message();
+								out.type = Message.OPPONENT_FORFEIT;
+								out.content = "NO_OPPONENT";
+								lastOpp.opponent = null;
+								lastOpp.send(out);
+								callback.accept("(Forfeit) Sent OPPONENT_FORFEIT to " + lastOpponentUsername);
+							}
+						}
+						opponent = null;
+						lastOpponentUsername = null;
+					}
+					callback.accept("(Forfeit) " + username + " went home.");
+					break;
+				}
+				case Message.GAME_MOVE: {
+					if (opponent != null) {
+						callback.accept("(Move) " + username + ": (" + data.fromRow + "," + data.fromCol + ") -> (" + data.toRow + "," + data.toCol + ")");
+						opponent.send(data);
+					}
+					break;
+				}
 				case Message.SEND_ALL: {
 					callback.accept("[Broadcast] " + data.sender + ": " + data.content);
-					Message deliverMsg = new Message();
-					deliverMsg.type = Message.CHAT_MESSAGE;
-					deliverMsg.content = "(All)" + data.sender + ": " + data.content;
-					for (ClientThread clientThread : clients) {
-						clientThread.send(deliverMsg);
-					}
+					Message out = new Message();
+					out.type = Message.CHAT_MESSAGE;
+					out.content = "(All)" + data.sender + ": " + data.content;
+					for (ClientThread ct : clients) ct.send(out);
 					break;
 				}
 
 				case Message.SEND_PRIVATE: {
 					callback.accept("(DM)" + data.sender + " -> " + data.recipient + ": " + data.content);
-					ClientThread targetClient = findClient(data.recipient);
-					Message deliverMsg = new Message();
-					deliverMsg.type = Message.CHAT_MESSAGE;
-					if (targetClient != null) {
-						deliverMsg.content = "(DM from " + data.sender + "): " + data.content;
-						targetClient.send(deliverMsg);
-						deliverMsg.content = "(DM to " + data.recipient + "): " + data.content;
-						send(deliverMsg);
-					} else {
-						deliverMsg.content = "(Server) User '" + data.recipient + "' not found.";
-						send(deliverMsg);
+					ClientThread target = findClient(data.recipient);
+					Message out = new Message();
+					out.type = Message.CHAT_MESSAGE;
+					if (target != null) {
+						out.content = data.sender + ": " + data.content;
+						target.send(out);
+						out.content = "You: " + data.content;
+						send(out);
+					}
+					else {
+						out.content = "(Server) User '" + data.recipient + "' not found.";
+						send(out);
 					}
 					break;
 				}
-
 				case Message.CREATE_GROUP: {
-					String newGroupName = data.groupName == null ? "" : data.groupName.trim();
+					String gName = data.groupName == null ? "" : data.groupName.trim();
 					Message reply = new Message();
 					reply.type = Message.CHAT_MESSAGE;
-						if (newGroupName.isEmpty()) {
-							reply.content = "(Server) Group name cannot be empty.";
-							send(reply);
-						} else if (groups.containsKey(newGroupName)) {
-							reply.content = "(Server) Group '" + newGroupName + "' already exists.";
-							send(reply);
-						} else {
-							Set<String> members = new HashSet<>();
-							members.add(data.sender);
-							groups.put(newGroupName, members);
-							callback.accept("(Group Created) '" + newGroupName + "' by " + data.sender);
-							reply.content = "(Server) Group '" + newGroupName + "' created. You are a member.";
-							send(reply);
-							broadcastGroupList();
-						}
+					if (gName.isEmpty()) {
+						reply.content = "(Server) Group name cannot be empty.";
+						send(reply);
+					}
+					else if (groups.containsKey(gName)) {
+						reply.content = "(Server) Group '" + gName + "' already exists.";
+						send(reply);
+					}
+					else {
+						Set<String> members = new HashSet<>();
+						members.add(data.sender);
+						groups.put(gName, members);
+						callback.accept("(Group Created) '" + gName + "' by " + data.sender);
+						reply.content = "(Server) Group '" + gName + "' created. You are a member.";
+						send(reply);
+						broadcastGroupList();
+					}
 					break;
 				}
-
 				case Message.JOIN_GROUP: {
-					String targetGroup = data.groupName;
+					String gName = data.groupName;
 					Message reply = new Message();
 					reply.type = Message.CHAT_MESSAGE;
-						if (!groups.containsKey(targetGroup)) {
-							reply.content = "(Server) Group '" + targetGroup + "' does not exist.";
-							send(reply);
-						} else if (groups.get(targetGroup).contains(data.sender)) {
-							reply.content = "(Server) You are already in group '" + targetGroup + "'.";
-							send(reply);
-						} else {
-							groups.get(targetGroup).add(data.sender);
-							callback.accept("(Group Join) " + data.sender + " joined '" + targetGroup + "'");
-							reply.content = "(Server) You joined group '" + targetGroup + "'.";
-							send(reply);
-							Message notifyMsg = new Message();
-							notifyMsg.type = Message.CHAT_MESSAGE;
-							notifyMsg.content = "(Group " + targetGroup + ") " + data.sender + " joined.";
-							for (String memberName : groups.get(targetGroup)) {
-								if (!memberName.equals(data.sender)) {
-									ClientThread memberThread = findClient(memberName);
-									if (memberThread != null) memberThread.send(notifyMsg);
-								}
+					if (!groups.containsKey(gName)) {
+						reply.content = "(Server) Group '" + gName + "' does not exist.";
+						send(reply);
+					}
+					else if (groups.get(gName).contains(data.sender)) {
+						reply.content = "(Server) You are already in group '" + gName + "'.";
+						send(reply);
+					}
+					else {
+						groups.get(gName).add(data.sender);
+						callback.accept("(Group Join) " + data.sender + " joined '" + gName + "'");
+						reply.content = "(Server) You joined group '" + gName + "'.";
+						send(reply);
+						Message notify = new Message();
+						notify.type    = Message.CHAT_MESSAGE;
+						notify.content = "(Group " + gName + ") " + data.sender + " joined.";
+						for (String m : groups.get(gName)) {
+							if (!m.equals(data.sender)) {
+								ClientThread mt = findClient(m);
+								if (mt != null) mt.send(notify);
 							}
 						}
+					}
 					break;
 				}
-
 				case Message.SEND_GROUP: {
-					String targetGroup = data.groupName;
-					callback.accept("(Group " + targetGroup + ") " + data.sender + ": " + data.content);
-					Message deliverMsg = new Message();
-					deliverMsg.type = Message.CHAT_MESSAGE;
-						if (!groups.containsKey(targetGroup)) {
-							deliverMsg.content = "(Server) Group '" + targetGroup + "' does not exist.";
-							send(deliverMsg);
-						} else if (!groups.get(targetGroup).contains(data.sender)) {
-							deliverMsg.content = "(Server) You are not a member of '" + targetGroup + "'.";
-							send(deliverMsg);
-						} else {
-							deliverMsg.content = "(Group " + targetGroup + ") " + data.sender + ": " + data.content;
-							for (String memberName : groups.get(targetGroup)) {
-								ClientThread memberThread = findClient(memberName);
-								if (memberThread != null) memberThread.send(deliverMsg);
-							}
+					String gName = data.groupName;
+					callback.accept("(Group " + gName + ") " + data.sender + ": " + data.content);
+					Message out = new Message();
+					out.type = Message.CHAT_MESSAGE;
+					if (!groups.containsKey(gName)) {
+						out.content = "(Server) Group '" + gName + "' does not exist.";
+						send(out);
+					}
+					else if (!groups.get(gName).contains(data.sender)) {
+						out.content = "(Server) You are not a member of '" + gName + "'.";
+						send(out);
+					}
+					else {
+						out.content = "(Group " + gName + ") " + data.sender + ": " + data.content;
+						for (String m : groups.get(gName)) {
+							ClientThread mt = findClient(m);
+							if (mt != null) mt.send(out);
 						}
+					}
 					break;
 				}
 			}
@@ -222,9 +368,25 @@ public class Server {
 
 		private void handleDisconnect() {
 			clients.remove(this);
+			synchronized (Server.this) {
+				matchQueue.remove(this);
+				rematchQueue.remove(username);
+				if (lastOpponentUsername != null) {
+					rematchQueue.remove(lastOpponentUsername);
+				}
+			}
 			if (username != null) {
 				takenUsernames.remove(username);
-				for (Set<String> members : groups.values()) members.remove(username);
+				for (Set<String> members : groups.values()){
+					members.remove(username);
+				}
+				if (opponent != null) {
+					Message out = new Message();
+					out.type = Message.GAME_OVER;
+					out.content = "WIN";
+					opponent.send(out);
+					opponent.opponent = null;
+				}
 				callback.accept("'" + username + "' disconnected.");
 				broadcastUserList();
 				broadcastGroupList();
